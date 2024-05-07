e18f6232401bcccd7b63fbbda336f3a2a4a44bc8
vivekanand.trivedi@hm.com <vivekanand99344074@gmail.com>
2024-05-07 10:06:36 +0530

    first day

---
+++ Demo1.java
@@ -0,0 +1,20 @@
+public class Demo1
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hi Welcome to Java  class ");
+	}
+}
+
+
+
+https://paste.ubuntu.com/p/5pc35gnYqc/
+
+
+
+
+
+
+
+
+
